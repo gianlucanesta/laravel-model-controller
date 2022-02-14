56ed3b0eb7eb08541b778a5be36c11ed566b1b85
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-02-14 16:17:12 +0100

    bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Movies</title>
 </head>
 <body>
@@ -11,18 +13,19 @@
     <h1>Lista Film</h1>
 
     {{-- {{dd($movies)}} --}}
-
-    @foreach ($movies as $movie)
-    {{-- {{ dd($movie->title)}} --}}
-    <div>
-        <h2>{{ $movie->title }}</h2>
-        <p><strong>Original title:</strong> {{ $movie ->original_title}}</p>
-        <p><strong>Nazionalità:</strong> {{ $movie->nationality }}</p>
-        <p><strong>Data:</strong> {{ $movie->date  }}</p>
-        <p><strong>Voto:</strong> {{ $movie->vote  }}</p>
+    <div class="container ">
+        <div class="cards">
+            @foreach ($movies as $movie)
+            {{-- {{ dd($movie->title)}} --}}
+                <div class="card">
+                    <h2>{{ $movie->title }}</h2>
+                    <p><strong>Original title:</strong> {{ $movie ->original_title}}</p>
+                    <p><strong>Nazionalità:</strong> {{ $movie->nationality }}</p>
+                    <p><strong>Data:</strong> {{ $movie->date  }}</p>
+                    <p><strong>Voto:</strong> {{ $movie->vote  }}</p>
+                </div>
+            @endforeach
+        </div>
     </div>
-    
-    @endforeach
-
 </body>
 </html>
